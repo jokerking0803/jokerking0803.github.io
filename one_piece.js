49f0b9dedaad185bd70adc65b9c73b08a48436ff
林家賢 <90121148+jokerking0803@users.noreply.github.com>
2023-11-06 13:59:57 +0800

    Add files via upload

--- one_piece.js
+++ one_piece.js
@@ -4,15 +4,18 @@ function toggleMode() {
   element.classList.toggle("night-mode");
 
   var modeToggle = document.getElementById("mode-toggle");
-  var elementToAdjust = document.querySelector("nav");
+  var elementToAdjust = document.querySelector(".nav1");
   elementToAdjust.classList.toggle("night-mode");
 
   if (element.classList.contains("night-mode")) {
     modeToggle.src = "/img/dark_mode_icon.png"; // 夜間模式按鈕圖片
     modeToggle.alt = "切換到日間模式";
+    modeToggle.nextSibling.textContent = "夜間模式"; // 更新按钮文字
   } else {
     modeToggle.src = "/img/light_mode_icon.png"; // 日間模式按鈕圖片
     modeToggle.alt = "切換到夜間模式";
+    modeToggle.nextSibling.textContent = "日間模式"; // 更新按钮文字
   }
 }
+    
 
